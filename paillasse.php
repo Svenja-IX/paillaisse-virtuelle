b16aa1d4d86e77ca23aa0a6aa31c102e4aa0c708
Telheb <garciaemmanuel@hotmail.com>
2020-06-03 15:10:10 +0200

    ajoustement css paillasse

--- paillasse.php
+++ paillasse.php
@@ -9,8 +9,25 @@
         <div class="placard"></div>
         <div class="placard"></div>
     </div>
-    <div class="vide"></div class="">
-    <div class="centre"></div>
-    <div class="bas"></div>
+
+    <div class="vide">
+        <div class="prise"></div>
+        <div class="prise"></div>
+        <div class="prise"></div>
+        <div></div>
+    </div class="">
+
+    <div class="centre">
+        <div class="planTravail"></div>
+        <div class="lavabo"></div>
+    </div>
+
+    <div class="bas">
+        <div class="placard-1"></div>
+        <div class="poubelle"></div>
+        <div class="poubelle"></div>
+        <div class="poubelle"></div>
+        <div class="placard-1"></div>
+    </div>
 </body>
 </html>
