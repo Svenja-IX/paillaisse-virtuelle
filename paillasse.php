04d6016cc65448db968db4fd6ea1d30c686da843
Telheb <garciaemmanuel@hotmail.com>
2020-06-03 11:47:19 +0200

    travail paillasse

--- paillasse.php
+++ paillasse.php
@@ -4,11 +4,20 @@
 ?>
 
 <body id="wrapper">
+
+
+
     <div id="paillasse">
+        <div id="placard">
+            <img id="b" class="draggable" src="img/bottle_test.png" alt="">
+        </div>
+        
         <figure id="sortir">
                 <a href="index.php"><img id="gate" src="img/gate.png" alt=""></a>
                 <figcaption>Sortir</figcaption>
         </figure>
     </div>
+
+    <script src= "scripts/reac.js"></script>
 </body>
 </html>
