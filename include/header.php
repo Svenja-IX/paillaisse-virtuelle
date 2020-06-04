e7ef31781602cdc9c8995a61685f767b77a0e755
Abad <axel.abad0901@gmail.com>
2020-06-04 15:21:35 +0200

    Drag fix + paillasse modifié

--- include/header.php
+++ include/header.php
@@ -3,12 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="scripts/script.js"></script>    
-    <link href="jquery-ui-1.10.4/css/ui-lightness/jquery-ui-1.10.4.css" rel="stylesheet">
-    <link href="jquery-ui-1.10.4/css/dev-facile/jquery-ui-1.10.4.custom.css" rel="stylesheet">
-    <link href="jquery-ui-1.10.4/css/dev-facile/jquery-ui-1.10.4.custom.css" rel="stylesheet">
-    <script src="jquery-ui-1.10.4/js/jquery-1.10.2.js"></script>
-    <script src="jquery-ui-1.10.4/js/jquery-ui-1.10.4.js"></script>
+       
 
     <?php require "css.php"; ?>
 
--- paillasse.php
+++ paillasse.php
@@ -6,7 +6,7 @@
 <body id="wrapper">
     <div class="top">
         <div class="placard">
-        <div id="drag1" class="drag-drop">
+        <div class="drag" class="ui-widget-content">
             <img src="img/L.png" alt="">
         </div>
         </div>
@@ -16,14 +16,22 @@
 
     <div class="vide">
         <div class="prise"></div>
-        <div class="prise"></div>
+        <div class="prise">
+       <?php 
+       if (false) {
+           echo '<img src="img/flammeOFF.png" alt="">';
+       } else {
+        echo '<img src="img/flammeON.png" alt="">';
+       }
+       ?>
+       </div>
         <div class="prise"></div>
         <div></div>
     </div class="">
 
     <div class="centre">
         <div class="planTravail">
-
+        
         </div>
         <div class="lavabo"></div>
     </div>
@@ -35,6 +43,8 @@
         <div class="poubelle"></div>
         <div class="placard-1"></div>
     </div>
- 
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="scripts/script.js"></script>
 </body>
 </html>
